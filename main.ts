cb51dcbbd9e3b27293692dce6725fbe6987bdca8
DragonzaS <87422096+DragonzaS@users.noreply.github.com>
2022-09-27 11:21:48 +0200

    Added speedcontrol to rainbow mode

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ let timer = 0
 let timer2 = 0
 let mode = 0
 let strip: neopixel.Strip = null
+let rainbowDelay = 100
 strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 let brightness = 127
 mode = 1
@@ -16,18 +17,30 @@ let warningshift = 1
 strip.showRainbow(1, 255)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        if (0 < brightness) {
-            brightness += -1
+        if (mode == 1) {
+            if (10 < rainbowDelay) {
+                rainbowDelay += -1
+            }
+        } else {
+            if (0 < brightness) {
+                brightness += -1
+            }
         }
     } else if (input.buttonIsPressed(Button.B)) {
-        if (brightness < 255) {
-            brightness += 1
+        if (mode == 1) {
+            if (rainbowDelay < 500) {
+                rainbowDelay += 1
+            }
+        } else {
+            if (brightness < 255) {
+                brightness += 1
+            }
         }
     }
     if (mode == 1) {
         if (timer2 == 0) {
             timer2 = control.millis()
-        } else if (control.millis() - timer2 > 50) {
+        } else if (control.millis() - timer2 > rainbowDelay) {
             timer2 = 0
             strip.rotate(1)
         }
